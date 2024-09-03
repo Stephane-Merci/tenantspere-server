345ecd105ca245aee660d3b55aefcdce117fd1f0
Nkengbeza Derick <nkengbderick@gmail.com>
2024-09-03 13:58:51 +0100

    swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("Tenant Sphere")
+    .setDescription("This is the Tenant Sphere API")
+    .setVersion('1.0')
+    .addTag('tenant sphere')
+    .build()
+    const document = SwaggerModule.createDocument(app, swaggerConfig)
+    SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.SERVER_PORT);
 }
 
